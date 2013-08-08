d84bc9ab3c57d39743872747b7ead4cf6004a0d7
Heiko Braun <ike.braun@googlemail.com>
2013-08-08 16:53:39 +0200

    fix notification styles

--- gui/src/main/java/org/jboss/as/console/client/core/Header.java
+++ gui/src/main/java/org/jboss/as/console/client/core/Header.java
@@ -183,7 +183,7 @@ public class Header implements ValueChangeHandler<String> {
         tools.add(principal);
 
         top.add(tools);
-        top.setWidgetRightWidth(tools, 15, Style.Unit.PX, 600, Style.Unit.PX);
+        top.setWidgetRightWidth(tools, 15, Style.Unit.PX, 700, Style.Unit.PX);
         top.setWidgetTopHeight(tools, 0, Style.Unit.PX, 32, Style.Unit.PX);
         top.setWidgetHorizontalPosition(tools, Layout.Alignment.END);
 
