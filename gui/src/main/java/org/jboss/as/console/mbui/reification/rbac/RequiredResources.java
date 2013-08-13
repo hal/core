095d97c4cb2f5c1f44a72281b56072d56944c8af
Heiko Braun <ike.braun@googlemail.com>
2013-08-13 11:05:14 +0200

    rbac utility for MBUI kernel

--- gui/src/main/java/org/jboss/as/console/mbui/reification/rbac/RequiredResources.java
+++ gui/src/main/java/org/jboss/as/console/mbui/reification/rbac/RequiredResources.java
@@ -1,8 +1,49 @@
 package org.jboss.as.console.mbui.reification.rbac;
 
+import org.jboss.as.console.mbui.model.StereoTypes;
+import org.jboss.as.console.mbui.model.mapping.DMRMapping;
+import org.useware.kernel.model.mapping.MappingType;
+import org.useware.kernel.model.structure.Container;
+import org.useware.kernel.model.structure.InteractionUnit;
+import org.useware.kernel.model.structure.builder.InteractionUnitVisitor;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Heiko Braun
  * @date 8/13/13
  */
-public class RequiredResources {
+public class RequiredResources implements InteractionUnitVisitor<StereoTypes>{
+
+    public Set<String> requiredresources = new HashSet<String>();
+
+    public Set<String> getRequiredresources() {
+        return requiredresources;
+    }
+
+    @Override
+    public void startVisit(Container container) {
+        inspectUnit(container);
+    }
+
+    @Override
+    public void visit(InteractionUnit<StereoTypes> interactionUnit) {
+        inspectUnit(interactionUnit);
+    }
+
+    @Override
+    public void endVisit(Container container) {
+
+    }
+
+    private void inspectUnit(InteractionUnit unit)
+    {
+        if(unit.hasMapping(MappingType.DMR))
+        {
+            DMRMapping mapping = (DMRMapping)unit.getMapping(MappingType.DMR);
+            if(mapping.getAddress()!=null)
+                requiredresources.add(mapping.getAddress());
+        }
+    }
 }
