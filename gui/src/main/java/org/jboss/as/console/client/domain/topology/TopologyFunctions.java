2d93759e1063a988ea7d0647a5e8aa8c76d0f5f9
Harald Pehl <harald.pehl@gmail.com>
2013-11-19 11:19:22 +0100

    HAL-308: Fix error when no running server is present

--- gui/src/main/java/org/jboss/as/console/client/domain/topology/TopologyFunctions.java
+++ gui/src/main/java/org/jboss/as/console/client/domain/topology/TopologyFunctions.java
@@ -215,35 +215,37 @@ public final class TopologyFunctions {
                 @Override
                 protected void onSuccess(final ModelNode result) {
                     ModelNode stepsResult = result.get(RESULT);
-                    List<Property> properties = stepsResult.asPropertyList();
-                    for (Iterator<Property> iterator = properties.iterator(); iterator.hasNext(); ) {
-                        Property property = iterator.next();
-                        String step = property.getName();
-                        ModelNode node = property.getValue();
-                        ServerInstance serverInstance = stepToServer.get(step);
-                        if (serverInstance != null) {
-                            // step-n: server state
-                            if (node.get(RESULT).isDefined()) {
-                                String state = node.get(RESULT).asString();
-                                if (state.equals("reload-required")) {
-                                    serverInstance.setFlag(ServerFlag.RELOAD_REQUIRED);
-                                } else if (state.equals("restart-required")) {
-                                    serverInstance.setFlag(ServerFlag.RESTART_REQUIRED);
+                    if (stepsResult.isDefined()) {
+                        List<Property> properties = stepsResult.asPropertyList();
+                        for (Iterator<Property> iterator = properties.iterator(); iterator.hasNext(); ) {
+                            Property property = iterator.next();
+                            String step = property.getName();
+                            ModelNode node = property.getValue();
+                            ServerInstance serverInstance = stepToServer.get(step);
+                            if (serverInstance != null) {
+                                // step-n: server state
+                                if (node.get(RESULT).isDefined()) {
+                                    String state = node.get(RESULT).asString();
+                                    if (state.equals("reload-required")) {
+                                        serverInstance.setFlag(ServerFlag.RELOAD_REQUIRED);
+                                    } else if (state.equals("restart-required")) {
+                                        serverInstance.setFlag(ServerFlag.RESTART_REQUIRED);
+                                    }
                                 }
-                            }
 
-                            // step-n + 1: socket binding groups
-                            property = iterator.next();
-                            node = property.getValue();
-                            if (node.get(RESULT).isDefined()) {
-                                List<Property> sockets = node.get(RESULT).asPropertyList();
-                                for (Property socket : sockets) {
-                                    serverInstance.getSocketBindings()
-                                            .put(socket.getName(), socket.getValue().get("port-offset").asString());
+                                // step-n + 1: socket binding groups
+                                property = iterator.next();
+                                node = property.getValue();
+                                if (node.get(RESULT).isDefined()) {
+                                    List<Property> sockets = node.get(RESULT).asPropertyList();
+                                    for (Property socket : sockets) {
+                                        serverInstance.getSocketBindings()
+                                                .put(socket.getName(), socket.getValue().get("port-offset").asString());
+                                    }
                                 }
                             }
                         }
-                    }
+                    } // else no running servers!
                     control.getContext().push(hosts);
                 }
             });
