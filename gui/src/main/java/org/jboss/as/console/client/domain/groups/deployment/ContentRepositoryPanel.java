cbdf158d976510476e521e0100cbb4810557b85a
Claudio Miranda <claudio@claudius.com.br>
2014-01-16 22:30:14 -0200

    removed labels for hash and date deployment

--- gui/src/main/java/org/jboss/as/console/client/domain/groups/deployment/ContentRepositoryPanel.java
+++ gui/src/main/java/org/jboss/as/console/client/domain/groups/deployment/ContentRepositoryPanel.java
@@ -109,15 +109,11 @@ public class ContentRepositoryPanel implements IsWidget
         form.setEnabled(false);
         TextAreaItem name = new TextAreaItem("name", "Name");
         TextAreaItem runtimeName = new TextAreaItem("runtimeName", "Runtime Name");
-        TextAreaItem hashPath = new TextAreaItem("hash", "Hash code");
-        TextAreaItem deployTime = new TextAreaItem("deployTime", "Date and time");
         final ListItem groups = new ListItem("assignments", "Assigned Groups");
-        form.setFields(name, runtimeName, hashPath, deployTime, groups);
+        form.setFields(name, runtimeName, groups);
 
         runtimeName.setEnabled(false);
-        deployTime.setEnabled(false);
         name.setEnabled(false);
-        hashPath.setEnabled(false);
         groups.setEnabled(false);
 
         form.bind(deploymentsTable);
