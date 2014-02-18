82e77d3ef2e12db9d056b057c4dace88fa8057d6
Harald Pehl <harald.pehl@gmail.com>
2014-02-18 12:26:40 +0100

    HAL-301: Add IDs for group navigation items

--- gui/src/main/java/org/jboss/as/console/client/widgets/tree/GroupItem.java
+++ gui/src/main/java/org/jboss/as/console/client/widgets/tree/GroupItem.java
@@ -4,10 +4,13 @@ import com.google.gwt.user.client.ui.TreeItem;
 
 public class GroupItem extends TreeItem {
 
+    private final static String ID_PREFIX = "nav_group_item_";
+
     public GroupItem(String title) {
         super();
 
         setText(title);
+        getElement().setId(ID_PREFIX + title.replace(' ', '_').toLowerCase());
         getElement().setAttribute("style", "cursor:pointer;");
     }
 
