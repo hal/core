4bc2c333753e2de244ff79e79241bb95e2bc6ac4
Heiko Braun <ike.braun@googlemail.com>
2014-03-06 16:31:12 +0100

    HAL-382: Prevent loading of google vis in web mode

--- gui/src/main/java/org/jboss/as/console/client/core/bootstrap/LoadGoogleViz.java
+++ gui/src/main/java/org/jboss/as/console/client/core/bootstrap/LoadGoogleViz.java
@@ -1,5 +1,6 @@
 package org.jboss.as.console.client.core.bootstrap;
 
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.visualization.client.VisualizationUtils;
 import com.google.gwt.visualization.client.visualizations.OrgChart;
 import com.google.gwt.visualization.client.visualizations.corechart.LineChart;
@@ -17,17 +18,19 @@ public class LoadGoogleViz implements Function<BootstrapContext>
     @Override
     public void execute(Control<BootstrapContext> control) {
 
-        VisualizationUtils.loadVisualizationApi(
-                new Runnable()
-                {
-                    @Override
-                    public void run()
+        if(!GWT.isScript())  // GWT vis is only used by MBUI tools. These are available in dev mode only
+        {
+            VisualizationUtils.loadVisualizationApi(
+                    new Runnable()
                     {
-                        System.out.println("Loaded Google Vizualization API");
-                    }
-                }, LineChart.PACKAGE, OrgChart.PACKAGE
-        );
-
+                        @Override
+                        public void run()
+                        {
+                            System.out.println("Loaded Google Vizualization API");
+                        }
+                    }, LineChart.PACKAGE, OrgChart.PACKAGE
+            );
+        }
         // viz can be loaded in background ...
         control.proceed();
     }
