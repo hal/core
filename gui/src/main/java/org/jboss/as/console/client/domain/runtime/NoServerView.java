1ac5cba9e55e3f4186170c4664a909b615e96140
Heiko Braun <ike.braun@googlemail.com>
2014-07-16 17:44:06 +0200

    pimp up NoServerView

--- gui/src/main/java/org/jboss/as/console/client/domain/runtime/NoServerView.java
+++ gui/src/main/java/org/jboss/as/console/client/domain/runtime/NoServerView.java
@@ -1,8 +1,15 @@
 package org.jboss.as.console.client.domain.runtime;
 
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Widget;
+import org.jboss.as.console.client.core.NameTokens;
 import org.jboss.as.console.client.core.SuspendableViewImpl;
+import org.jboss.as.console.client.layout.SimpleLayout;
+import org.jboss.as.console.client.shared.homepage.ContentBox;
+import org.jboss.as.console.client.tools.UUID;
+import org.jboss.ballroom.client.widgets.ContentHeaderLabel;
 
 /**
  * @author Heiko Braun
@@ -10,8 +17,36 @@ import org.jboss.as.console.client.core.SuspendableViewImpl;
  */
 public class NoServerView extends SuspendableViewImpl implements NoServerPresenter.MyView {
 
+
+    private ContentHeaderLabel header;
+
     @Override
     public Widget createWidget() {
-        return new HTML("No Server Selected");
+
+        header = new ContentHeaderLabel();
+
+
+        SafeHtmlBuilder createServerDesc = new SafeHtmlBuilder();
+        createServerDesc.appendEscaped("A server configuration does specify the overall configuration of a server. A server configuration can be started and perform work.").appendHtmlConstant("<p/>");
+
+        ContentBox createServer = new ContentBox(
+                UUID.uuid(), "Create Server",
+                createServerDesc.toSafeHtml(),
+                "Server Configurations", NameTokens.ServerPresenter
+        );
+
+        SimpleLayout layout = new SimpleLayout()
+                .setHeadlineWidget(header)
+                .setPlain(true)
+                .setDescription("It seems there is no server configured for this host.")
+                .addContent("", createServer);
+
+        return layout.build();
+    }
+
+
+    @Override
+    public void setHostName(String selectedHost) {
+        header.setText("No server configured for host '"+selectedHost+"'");
     }
 }
