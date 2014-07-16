e335318ed412b3eb83732d8e8e42b025940e5076
Heiko Braun <ike.braun@googlemail.com>
2014-07-16 17:44:06 +0200

    pimp up NoServerView

--- gui/src/main/java/org/jboss/as/console/client/domain/runtime/NoServerPresenter.java
+++ gui/src/main/java/org/jboss/as/console/client/domain/runtime/NoServerPresenter.java
@@ -10,9 +10,11 @@ import com.gwtplatform.mvp.client.proxy.Place;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
 import com.gwtplatform.mvp.client.proxy.Proxy;
 import com.gwtplatform.mvp.client.proxy.RevealContentEvent;
+import org.jboss.as.console.client.Console;
 import org.jboss.as.console.client.core.MainLayoutPresenter;
 import org.jboss.as.console.client.core.NameTokens;
 import org.jboss.as.console.client.shared.subsys.RevealStrategy;
+import org.jboss.as.console.client.v3.stores.domain.HostStore;
 import org.jboss.as.console.client.v3.stores.domain.ServerStore;
 
 /**
@@ -21,6 +23,8 @@ import org.jboss.as.console.client.v3.stores.domain.ServerStore;
  */
 public class NoServerPresenter extends Presenter<NoServerPresenter.MyView, NoServerPresenter.MyProxy> {
 
+    private final ServerStore serverStore;
+    private final HostStore hostStore;
     private RevealStrategy revealStrategy;
 
     @ProxyCodeSplit
@@ -29,21 +33,32 @@ public class NoServerPresenter extends Presenter<NoServerPresenter.MyView, NoSer
     }
 
     public interface MyView extends View {
+        void setHostName(String selectedHost);
     }
 
     @Inject
     public NoServerPresenter(
             EventBus eventBus, MyView view, MyProxy proxy,
-            RevealStrategy revealStrategy, ServerStore serverStore, PlaceManager placeManager) {
+            RevealStrategy revealStrategy, ServerStore serverStore, PlaceManager placeManager, HostStore hostStore) {
 
         super(eventBus, view, proxy);
 
         this.revealStrategy = revealStrategy;
+        this.serverStore = serverStore;
+        this.hostStore = hostStore;
+    }
+
+    @Override
+    protected void onReset() {
+        super.onReset();
+        Console.MODULES.getHeader().highlight(NameTokens.DomainRuntimePresenter);
+        getView().setHostName(hostStore.getSelectedHost());
     }
 
     @Override
     protected void revealInParent() {
         RevealContentEvent.fire(this, MainLayoutPresenter.TYPE_MainContent, this);
+        //revealStrategy.revealInRuntimeParent(this);
     }
 
 }
