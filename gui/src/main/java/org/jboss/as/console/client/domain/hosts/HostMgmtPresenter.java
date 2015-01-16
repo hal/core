4990804e0493fac9bc5251c393a86760dfe548f7
Heiko Braun <ike.braun@googlemail.com>
2015-01-16 20:04:41 +0100

    Keep server config functionality within ServerConfigPresenter

--- gui/src/main/java/org/jboss/as/console/client/domain/hosts/HostMgmtPresenter.java
+++ gui/src/main/java/org/jboss/as/console/client/domain/hosts/HostMgmtPresenter.java
@@ -38,19 +38,15 @@ import org.jboss.as.console.client.core.BootstrapContext;
 import org.jboss.as.console.client.core.Header;
 import org.jboss.as.console.client.core.MainLayoutPresenter;
 import org.jboss.as.console.client.core.NameTokens;
-import org.jboss.as.console.client.domain.model.Server;
 import org.jboss.as.console.client.rbac.UnauthorisedPresenter;
 import org.jboss.as.console.client.shared.state.PerspectivePresenter;
 import org.jboss.as.console.client.v3.stores.domain.HostStore;
-import org.jboss.as.console.client.v3.stores.domain.ServerStore;
 import org.jboss.as.console.client.v3.stores.domain.actions.RefreshHosts;
 import org.jboss.as.console.spi.SearchIndex;
 import org.jboss.gwt.circuit.Action;
 import org.jboss.gwt.circuit.Dispatcher;
 import org.jboss.gwt.circuit.PropagatesChange;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -58,7 +54,6 @@ import java.util.Set;
  */
 public class HostMgmtPresenter extends PerspectivePresenter<HostMgmtPresenter.MyView, HostMgmtPresenter.MyProxy> {
 
-    private HandlerRegistration serverHandler;
 
     @ProxyCodeSplit
     @NameToken(NameTokens.HostMgmtPresenter)
@@ -71,15 +66,12 @@ public class HostMgmtPresenter extends PerspectivePresenter<HostMgmtPresenter.My
     public interface MyView extends View {
         void setPresenter(HostMgmtPresenter presenter);
         void updateHosts(String selectedHost, Set<String> hostNames);
-
-        void updateServer(List<Server> serverModel);
     }
 
 
     @ContentSlot
     public static final GwtEvent.Type<RevealContentHandler<?>> TYPE_MainContent = new GwtEvent.Type<RevealContentHandler<?>>();
 
-    private final ServerStore serverStore;
     private final Dispatcher circuit;
     private BootstrapContext bootstrap;
     private final HostStore hostStore;
@@ -88,7 +80,7 @@ public class HostMgmtPresenter extends PerspectivePresenter<HostMgmtPresenter.My
 
     @Inject
     public HostMgmtPresenter(EventBus eventBus, MyView view, MyProxy proxy, PlaceManager placeManager,
-            BootstrapContext bootstrap, Header header, HostStore hostStore, ServerStore serverStore, Dispatcher circuit,
+            BootstrapContext bootstrap, Header header, HostStore hostStore, Dispatcher circuit,
             UnauthorisedPresenter unauthorisedPresenter) {
 
         super(eventBus, view, proxy, placeManager, header, NameTokens.HostMgmtPresenter, unauthorisedPresenter,
@@ -96,7 +88,6 @@ public class HostMgmtPresenter extends PerspectivePresenter<HostMgmtPresenter.My
 
         this.bootstrap = bootstrap;
         this.hostStore = hostStore;
-        this.serverStore = serverStore;
         this.circuit = circuit;
     }
 
@@ -113,22 +104,6 @@ public class HostMgmtPresenter extends PerspectivePresenter<HostMgmtPresenter.My
 
                 getView().updateHosts(hostStore.getSelectedHost(), hostStore.getHostNames());
 
-            }
-        });
-
-        serverHandler = hostStore.addChangeHandler(new PropagatesChange.Handler() {
-            @Override
-            public void onChange(Action action) {
-
-                String selectedHost = hostStore.getSelectedHost();
-                List<Server> serverModel = Collections.EMPTY_LIST;
-                if (selectedHost != null) {
-                    serverModel = serverStore.getServerModel(
-                            hostStore.getSelectedHost()
-                    );
-
-                }
-                getView().updateServer(serverModel);
             }
         });
 
