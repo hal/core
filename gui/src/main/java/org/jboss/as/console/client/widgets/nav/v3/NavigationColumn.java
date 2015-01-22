584e400d8f2fc832cda014b837573d1ef22350ff
Heiko Braun <ike.braun@googlemail.com>
2015-01-22 17:42:22 +0100

    Turn the context menu into a visible property

--- gui/src/main/java/org/jboss/as/console/client/widgets/nav/v3/NavigationColumn.java
+++ gui/src/main/java/org/jboss/as/console/client/widgets/nav/v3/NavigationColumn.java
@@ -1,17 +1,27 @@
 package org.jboss.as.console.client.widgets.nav.v3;
 
 import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.cell.client.Cell;
+import com.google.gwt.cell.client.SafeHtmlCell;
 import com.google.gwt.cell.client.ValueUpdater;
 import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.dom.builder.shared.TableSectionBuilder;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.TableRowElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
-import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.user.cellview.client.Header;
+import com.google.gwt.user.cellview.client.HeaderBuilder;
+import com.google.gwt.user.cellview.client.RowHoverEvent;
+import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.LayoutPanel;
@@ -19,12 +29,14 @@ import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.MenuItem;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.CellPreviewEvent;
 import com.google.gwt.view.client.ProvidesKey;
 import com.google.gwt.view.client.SelectionChangeEvent;
 import com.google.gwt.view.client.SingleSelectionModel;
-import org.jboss.ballroom.client.widgets.tools.ToolStrip;
+import org.jboss.as.console.client.widgets.tables.IconCell;
+import org.jboss.ballroom.client.widgets.tables.DefaultCellTable;
+import org.jboss.dmr.client.ModelNode;
 
 import java.util.List;
 
@@ -36,7 +48,8 @@ public class NavigationColumn<T> {
 
 
     private final SingleSelectionModel<T> selectionModel;
-    private final CellList<T> cellList;
+    //private final CellList<T> cellList;
+    private final CellTable<T> cellTable;
     private final String title;
     private final ProvidesKey keyProvider;
     private HorizontalPanel header;
@@ -45,75 +58,105 @@ public class NavigationColumn<T> {
     private MenuDelegate[] topMenuItems = new MenuDelegate[]{};
     private HTML headerTitle;
 
-    public NavigationColumn(String title, final Display display, ProvidesKey keyProvider) {
+    public NavigationColumn(String title, final Display display, final ProvidesKey keyProvider) {
         this.title = title;
         this.keyProvider = keyProvider;
         selectionModel = new SingleSelectionModel<T>();
 
-        cellList = new CellList<T>(new AbstractCell<T>("contextmenu")
-        {
+        cellTable = new CellTable<T>(200, DefaultCellTable.DEFAULT_CELL_TABLE_RESOURCES , keyProvider);
+        cellTable.setStyleName("navigation-cell-table");
+        cellTable.getElement().setAttribute("style", "border:none!important");
 
+        Column<T, SafeHtml> titleColumn = new Column<T, SafeHtml>(new SafeHtmlCell()) {
             @Override
-            public void render(Context context, T data, SafeHtmlBuilder sb) {
-                String cssName = (context.getIndex() %2 > 0) ? "combobox-item combobox-item-odd" : "combobox-item";
-
-                if(data.equals(selectionModel.getSelectedObject()))
-                    cssName+=" combobox-item-selected";
-
-                sb.append(display.render(cssName, data));
+            public SafeHtml getValue(T data) {
+                return display.render("navigation-column-item", data);
             }
+        };
 
+        Column<T, SafeHtml> menuColumn = new Column<T, SafeHtml>(new SafeHtmlCell()) {
             @Override
-            public void onBrowserEvent(Context context, Element parent, final T value, NativeEvent event, ValueUpdater<T> valueUpdater) {
-                String eventType = event.getType();
+            public SafeHtml getValue(T data) {
+                SafeHtmlBuilder builder = new SafeHtmlBuilder();
+                builder.appendHtmlConstant("<span class='nav-menu'><i class='icon-angle-down'></i></span>");
+                return builder.toSafeHtml();
+            }
+        };
 
-                if ("contextmenu".equals(eventType)) {
+        cellTable.addColumn(titleColumn);
+        cellTable.addColumn(menuColumn);
 
-                    event.preventDefault();
+        cellTable.setColumnWidth(menuColumn, 20, Style.Unit.PX);
 
-                    final PopupPanel popupPanel = new PopupPanel(true);
+        cellTable.setSelectionModel(selectionModel);
 
-                    final MenuBar popupMenuBar = new MenuBar(true);
+        // visibility of the context menu column
+        cellTable.addRowHoverHandler(new RowHoverEvent.Handler() {
+            @Override
+            public void onRowHover(RowHoverEvent event) {
+                TableRowElement hoveringRow = event.getHoveringRow();
+                if(event.isUnHover()) {
+                    hoveringRow.removeClassName("nav-hover");
+                }
+                else
+                {
+                    hoveringRow.addClassName("nav-hover");
+                }
 
-                    for (final MenuDelegate menuitem : menuItems) {
-                        MenuItem cmd  = new MenuItem(menuitem.getTitle(), true,  new Command() {
+            }
+        });
 
-                            @Override
-                            public void execute() {
-                                Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
-                                    @Override
-                                    public void execute() {
-                                        menuitem.getCommand().executeOn((T) value);
-                                    }
-                                });
-
-                                popupPanel.hide();
-                            }
-
-                        });
-
-                        popupMenuBar.addItem(cmd);
-                    }
-
-                    popupMenuBar.setVisible(true);
-
-
-                    popupPanel.add(popupMenuBar);
-                    popupPanel.setPopupPosition(event.getClientX(), event.getClientY());
-                    popupPanel.show();
-
+        cellTable.addCellPreviewHandler(new CellPreviewEvent.Handler<T>() {
+            @Override
+            public void onCellPreview(CellPreviewEvent<T> event) {
+                boolean isClick = "click".equals(event.getNativeEvent().getType());
+                if(isClick && 1==event.getColumn())
+                {
+                    event.getNativeEvent().preventDefault();
+                    openContextMenu(event.getNativeEvent(), event.getValue());
                 }
             }
         });
 
-        cellList.setSelectionModel(selectionModel);
+    }
 
+    private void openContextMenu(NativeEvent event, final T object) {
+        final PopupPanel popupPanel = new PopupPanel(true);
+
+        final MenuBar popupMenuBar = new MenuBar(true);
+
+        for (final MenuDelegate menuitem : menuItems) {
+            MenuItem cmd  = new MenuItem(menuitem.getTitle(), true,  new Command() {
+
+                @Override
+                public void execute() {
+                    Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+                        @Override
+                        public void execute() {
+                            menuitem.getCommand().executeOn((T) object);
+                        }
+                    });
+
+                    popupPanel.hide();
+                }
+
+            });
+
+            popupMenuBar.addItem(cmd);
+        }
+
+        popupMenuBar.setVisible(true);
+
+
+        popupPanel.add(popupMenuBar);
+        popupPanel.setPopupPosition(event.getClientX()-5, event.getClientY()-5);
+        popupPanel.show();
     }
 
     public NavigationColumn<T> setTopMenuItems(MenuDelegate... items) {
-           this.topMenuItems = items;
-           return this;
-       }
+        this.topMenuItems = items;
+        return this;
+    }
 
     public NavigationColumn<T> setMenuItems(MenuDelegate... items) {
         this.menuItems = items;
@@ -151,7 +194,7 @@ public class NavigationColumn<T> {
             headerTitle = new HTML(title);
             headerTitle.getElement().setAttribute("style", "height:25px");
             header.add(headerTitle);
-            ScrollPanel nav = new ScrollPanel(cellList);
+            ScrollPanel nav = new ScrollPanel(cellTable);
 
 
             for (final MenuDelegate menuItem : topMenuItems) {
@@ -160,7 +203,7 @@ public class NavigationColumn<T> {
                 item.addClickHandler(new ClickHandler() {
                     @Override
                     public void onClick(ClickEvent event) {
-                       menuItem.getCommand().executeOn(null);
+                        menuItem.getCommand().executeOn(null);
                     }
                 });
                 header.add(item);
@@ -176,7 +219,7 @@ public class NavigationColumn<T> {
         }
         else            // embedded mode, w/o header
         {
-            ScrollPanel nav = new ScrollPanel(cellList);
+            ScrollPanel nav = new ScrollPanel(cellTable);
 
             layout.add(nav);
             layout.setWidgetTopHeight(nav, 0, Style.Unit.PX, 100, Style.Unit.PCT);
@@ -192,8 +235,8 @@ public class NavigationColumn<T> {
 
     public void updateFrom(final List<T> records, final boolean selectDefault) {
         selectionModel.clear();
-        cellList.setRowCount(records.size(), true);
-        cellList.setRowData(0, records);
+        cellTable.setRowCount(records.size(), true);
+        cellTable.setRowData(0, records);
 
         if(!plain) headerTitle.setHTML(title+" ("+records.size()+")");
         if(selectDefault && records.size()>0)
@@ -214,10 +257,10 @@ public class NavigationColumn<T> {
     public void selectByKey(Object key) {
         selectionModel.clear();
         int i=0;
-        for(T item : cellList.getVisibleItems()) {
+        for(T item : cellTable.getVisibleItems()) {
             if(keyProvider.getKey(item).equals(key)) {
                 selectionModel.setSelected(item, true);
-                cellList.getRowElement(i).scrollIntoView();
+                cellTable.getRowElement(i).scrollIntoView();
                 break;
             }
             i++;
