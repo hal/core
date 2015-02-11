340f1ab3ee6eb5a186c0c391b227e4cb81e8601f
Heiko Braun <ike.braun@googlemail.com>
2015-02-11 12:03:36 +0100

    Runtime example breakdown for finder structure

--- gui/src/main/java/org/jboss/as/console/client/domain/runtime/DomainRuntimeView.java
+++ gui/src/main/java/org/jboss/as/console/client/domain/runtime/DomainRuntimeView.java
@@ -5,7 +5,7 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.safehtml.client.SafeHtmlTemplates;
 import com.google.gwt.safehtml.shared.SafeHtml;
-import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.SplitLayoutPanel;
@@ -13,7 +13,6 @@ import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.ProvidesKey;
 import com.google.gwt.view.client.SelectionChangeEvent;
 import com.gwtplatform.mvp.client.ViewImpl;
-import com.gwtplatform.mvp.shared.proxy.PlaceRequest;
 import org.jboss.as.console.client.Console;
 import org.jboss.as.console.client.core.NameTokens;
 import org.jboss.as.console.client.core.message.Message;
@@ -24,27 +23,33 @@ import org.jboss.as.console.client.plugins.RuntimeGroup;
 import org.jboss.as.console.client.shared.model.SubsystemRecord;
 import org.jboss.as.console.client.v3.stores.domain.actions.FilterType;
 import org.jboss.as.console.client.v3.stores.domain.actions.SelectServer;
-import org.jboss.as.console.client.widgets.nav.v3.ContextualCommand;
-import org.jboss.as.console.client.widgets.nav.v3.MenuDelegate;
 import org.jboss.as.console.client.widgets.nav.v3.NavigationColumn;
 
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author Heiko Braun
  */
 public class DomainRuntimeView extends ViewImpl implements DomainRuntimePresenter.MyView {
 
+    private final SplitLayoutPanel splitlayout;
+    private  Widget subsysColWidget;
     private LayoutPanel contentCanvas;
     private NavigationColumn<Server> serverColumn;
     private DomainRuntimePresenter presenter;
-    private NavigationColumn<RuntimeLink> subsystemColumn;
+    private NavigationColumn<ActionLink> statusColumn;
+    private NavigationColumn<PlaceLink> subsystemColumn;
 
     private List<Predicate> metricPredicates = new ArrayList<Predicate>();
     private List<Predicate> runtimePredicates = new ArrayList<Predicate>();
-    private List<RuntimeLink> platformLinks = new ArrayList<RuntimeLink>();
+    private List<ActionLink> statusLinks = new ArrayList<ActionLink>();
+    private List<SubsystemRecord> subsystems;
+    private Widget statusColWidget;
+
+    Stack<Widget> visibleColumns = new Stack<>();
 
     interface ServerTemplate extends SafeHtmlTemplates {
         @Template("<div class=\"{0}\">{1}&nbsp;<span style='font-size:8px'>({2})</span></div>")
@@ -56,7 +61,6 @@ public class DomainRuntimeView extends ViewImpl implements DomainRuntimePresente
         SafeHtml item(String cssClass, String server);
     }
 
-
     private static final ServerTemplate SERVER_TEMPLATE = GWT.create(ServerTemplate.class);
 
     private static final SubsystemTemplate SUBSYSTEM_TEMPLATE = GWT.create(SubsystemTemplate.class);
@@ -65,13 +69,14 @@ public class DomainRuntimeView extends ViewImpl implements DomainRuntimePresente
     public DomainRuntimeView() {
         super();
         contentCanvas = new LayoutPanel();
+        splitlayout = new SplitLayoutPanel(2);
 
-        RuntimeLink datasources = new RuntimeLink("Datasources", NameTokens.DataSourceMetricPresenter);
-        RuntimeLink jmsQueues = new RuntimeLink("JMS Destinations", NameTokens.JmsMetricPresenter);
-        RuntimeLink web = new RuntimeLink("Web", NameTokens.WebMetricPresenter);
-        RuntimeLink jpa = new RuntimeLink("JPA", NameTokens.JPAMetricPresenter);
-        RuntimeLink ws = new RuntimeLink("Webservices", NameTokens.WebServiceRuntimePresenter);
-        RuntimeLink naming = new RuntimeLink("JNDI View", NameTokens.JndiPresenter);
+        PlaceLink datasources = new PlaceLink("Datasources", NameTokens.DataSourceMetricPresenter);
+        PlaceLink jmsQueues = new PlaceLink("JMS Destinations", NameTokens.JmsMetricPresenter);
+        PlaceLink web = new PlaceLink("Web", NameTokens.WebMetricPresenter);
+        PlaceLink jpa = new PlaceLink("JPA", NameTokens.JPAMetricPresenter);
+        PlaceLink ws = new PlaceLink("Webservices", NameTokens.WebServiceRuntimePresenter);
+        PlaceLink naming = new PlaceLink("JNDI View", NameTokens.JndiPresenter);
 
         metricPredicates.add(new Predicate("datasources", datasources));
         metricPredicates.add(new Predicate("messaging", jmsQueues));
@@ -89,7 +94,7 @@ public class DomainRuntimeView extends ViewImpl implements DomainRuntimePresente
             {
                 metricPredicates.add(
                         new Predicate(
-                                ext.getKey(), new RuntimeLink(ext.getName(), ext.getToken())
+                                ext.getKey(), new PlaceLink(ext.getName(), ext.getToken())
                         )
                 );
             }
@@ -97,7 +102,7 @@ public class DomainRuntimeView extends ViewImpl implements DomainRuntimePresente
             {
                 runtimePredicates.add(
                         new Predicate(
-                                ext.getKey(), new RuntimeLink(ext.getName(), ext.getToken())
+                                ext.getKey(), new PlaceLink(ext.getName(), ext.getToken())
                         )
                 );
             }
@@ -108,16 +113,44 @@ public class DomainRuntimeView extends ViewImpl implements DomainRuntimePresente
         }
 
         // default links
-        platformLinks.add(new RuntimeLink("JVM", NameTokens.HostVMMetricPresenter));
-        platformLinks.add(new RuntimeLink("Environment", NameTokens.EnvironmentPresenter));
-        platformLinks.add(new RuntimeLink("Log Viewer", NameTokens.LogFiles));
+        statusLinks.add(new ActionLink("JVM", new Command() {
+            @Override
+            public void execute() {
+                reduceColumnsTo(2);
+                // NameTokens.HostVMMetricPresenter
+            }
+        }));
+        statusLinks.add(new ActionLink("Environment", new Command() {
+            @Override
+            public void execute() {
+                reduceColumnsTo(2);
+                // NameTokens.EnvironmentPresenter
+            }
+        }));
+        statusLinks.add(new ActionLink("Log Files", new Command() {
+            @Override
+            public void execute() {
+                reduceColumnsTo(2);
+                // NameTokens.LogFiles
+            }
+        }));
+
+
+        statusLinks.add(new ActionLink("Subsystems", new Command() {
+            @Override
+            public void execute() {
+                reduceColumnsTo(2);
+                appendColumn(subsysColWidget);
+                updateSubsystemColumn(subsystems);
+            }
+        }));
 
     }
 
     @Override
     public Widget asWidget() {
 
-        SplitLayoutPanel splitlayout = new SplitLayoutPanel(2);
+
 
         serverColumn = new NavigationColumn<Server>(
                 "Server",
@@ -136,35 +169,43 @@ public class DomainRuntimeView extends ViewImpl implements DomainRuntimePresente
                 });
 
 
-        subsystemColumn = new NavigationColumn<RuntimeLink>(
-                "Views",
-                new NavigationColumn.Display<RuntimeLink>() {
+        statusColumn = new NavigationColumn<ActionLink>(
+                "Status",
+                new NavigationColumn.Display<ActionLink>() {
                     @Override
-                    public SafeHtml render(String baseCss, RuntimeLink data) {
+                    public SafeHtml render(String baseCss, ActionLink data) {
                         return SUBSYSTEM_TEMPLATE.item(baseCss, data.getTitle());
                     }
                 },
-                new ProvidesKey<RuntimeLink>() {
+                new ProvidesKey<ActionLink>() {
                     @Override
-                    public Object getKey(RuntimeLink item) {
+                    public Object getKey(ActionLink item) {
+                        return item.getTitle();
+                    }
+                });
+
+        statusColWidget = statusColumn.asWidget();
+
+        subsystemColumn = new NavigationColumn<PlaceLink>(
+                "Subsystems",
+                new NavigationColumn.Display<PlaceLink>() {
+                    @Override
+                    public SafeHtml render(String baseCss, PlaceLink data) {
+                        return SUBSYSTEM_TEMPLATE.item(baseCss, data.getTitle());
+                    }
+                },
+                new ProvidesKey<PlaceLink>() {
+                    @Override
+                    public Object getKey(PlaceLink item) {
                         return item.getTitle();
                     }
                 });
 
 
-        /*serverColumn.setTopMenuItems(
-                new MenuDelegate<Server>(
-                        "<i class=\"icon-refresh\" style='color:black'></i>&nbsp;Refresh", new ContextualCommand<Server>() {
-                    @Override
-                    public void executeOn(Server server) {
-                        // TODO
-                    }
-                })
-        );*/
+        subsysColWidget = subsystemColumn.asWidget();
 
-        splitlayout.addWest(serverColumn.asWidget(), 217);
-        splitlayout.addWest(subsystemColumn.asWidget(), 217);
-        splitlayout.add(contentCanvas);
+        // server column is always present
+        appendColumn(serverColumn.asWidget());
 
         // selection handling
 
@@ -176,6 +217,8 @@ public class DomainRuntimeView extends ViewImpl implements DomainRuntimePresente
 
                     final Server selectedServer = serverColumn.getSelectedItem();
 
+                    reduceColumnsTo(1);
+                    appendColumn(statusColWidget);
                     Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
                         public void execute() {
 
@@ -188,26 +231,54 @@ public class DomainRuntimeView extends ViewImpl implements DomainRuntimePresente
             }
         });
 
+        statusColumn.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
+            @Override
+            public void onSelectionChange(SelectionChangeEvent event) {
+
+                if (statusColumn.hasSelectedItem()) {
+
+                    final ActionLink selectedLink = statusColumn.getSelectedItem();
+                     selectedLink.getCmd().execute();
+                }
+            }
+        });
+
+
         subsystemColumn.addSelectionChangeHandler(new SelectionChangeEvent.Handler() {
-                  @Override
-                  public void onSelectionChange(SelectionChangeEvent event) {
+            @Override
+            public void onSelectionChange(SelectionChangeEvent event) {
 
-                      if (subsystemColumn.hasSelectedItem()) {
+                if (subsystemColumn.hasSelectedItem()) {
 
-                          final RuntimeLink selectedLink = subsystemColumn.getSelectedItem();
+                    final PlaceLink selectedLink = subsystemColumn.getSelectedItem();
 
-                          Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
-                              public void execute() {
+                  /*  Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
+                        public void execute() {
 
-                                  Console.getPlaceManager().revealPlace(new PlaceRequest(selectedLink.getToken()));
-                              }
-                          });
-                      }
-                  }
-              });
+                            Console.getPlaceManager().revealPlace(new PlaceRequest(selectedLink.getToken()));
+                        }
+                    });*/
+                }
+            }
+        });
 
 
         return splitlayout.asWidget();
+    }
+
+    private void appendColumn(Widget columnWidget) {
+        visibleColumns.push(columnWidget);
+        splitlayout.addWest(columnWidget, 217);
+    }
+
+    private void reduceColumnsTo(int level) {
+
+        for(int i=visibleColumns.size()-1; i>=level; i--)
+        {
+            Widget widget = visibleColumns.pop();
+            boolean b = splitlayout.remove(widget);
+            System.out.println(i+" popped? "+b);
+        }
     }
 
 
@@ -237,15 +308,16 @@ public class DomainRuntimeView extends ViewImpl implements DomainRuntimePresente
 
     @Override
     public void updateServerList(List<Server> serverModel) {
-        serverColumn.updateFrom(serverModel, true);
+        reduceColumnsTo(1);
+        serverColumn.updateFrom(serverModel, false);
     }
 
-    private class RuntimeLink {
+    private class PlaceLink {
 
         private String title;
         private String token;
 
-        public RuntimeLink(String title, String token) {
+        public PlaceLink(String title, String token) {
             this.title = title;
             this.token = token;
         }
@@ -259,11 +331,30 @@ public class DomainRuntimeView extends ViewImpl implements DomainRuntimePresente
         }
     }
 
+    private class ActionLink {
+
+        private String title;
+        private Command cmd;
+
+        public ActionLink(String title, Command cmd) {
+            this.title = title;
+            this.cmd = cmd;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Command getCmd() {
+            return cmd;
+        }
+    }
+
     public final class Predicate {
         private String subsysName;
-        private RuntimeLink link;
+        private PlaceLink link;
 
-        public Predicate(String subsysName, RuntimeLink navItem) {
+        public Predicate(String subsysName, PlaceLink navItem) {
             this.subsysName = subsysName;
             this.link = navItem;
         }
@@ -272,7 +363,7 @@ public class DomainRuntimeView extends ViewImpl implements DomainRuntimePresente
             return current.equals(subsysName);
         }
 
-        public RuntimeLink getLink() {
+        public PlaceLink getLink() {
             return link;
         }
     }
@@ -280,7 +371,14 @@ public class DomainRuntimeView extends ViewImpl implements DomainRuntimePresente
     @Override
     public void setSubsystems(List<SubsystemRecord> subsystems) {
 
-        List<RuntimeLink> runtimeLinks = new ArrayList<>(platformLinks);
+        statusColumn.updateFrom(statusLinks);
+
+        this.subsystems = subsystems;
+    }
+
+    private void updateSubsystemColumn(List<SubsystemRecord> subsystems)
+    {
+        List<PlaceLink> runtimeLinks = new ArrayList<>();
 
         for(SubsystemRecord subsys : subsystems)
         {
@@ -292,7 +390,7 @@ public class DomainRuntimeView extends ViewImpl implements DomainRuntimePresente
             }
         }
 
-        subsystemColumn.updateFrom(runtimeLinks, true);
+        subsystemColumn.updateFrom(runtimeLinks, false);
     }
 
 }
