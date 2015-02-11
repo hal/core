70a4d8e29cb9b4459e07c1ebd5bd104f101a26c0
Heiko Braun <ike.braun@googlemail.com>
2015-02-11 17:39:18 +0100

    Clear active selection event

---
+++ gui/src/main/java/org/jboss/as/console/client/widgets/nav/v3/ClearFinderSelectionEvent.java
@@ -0,0 +1,54 @@
+package org.jboss.as.console.client.widgets.nav.v3;
+
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.HasHandlers;
+
+
+/**
+ * Remove the "active" selection css form relevant elements in the finder.
+ */
+public class ClearFinderSelectionEvent extends GwtEvent<ClearFinderSelectionEvent.Handler> {
+
+    public static final Type TYPE = new Type<Handler>();
+
+
+    public static void fire(final HasHandlers source) {
+        source.fireEvent(new ClearFinderSelectionEvent());
+    }
+
+    @Override
+    public Type<Handler> getAssociatedType() {
+        return TYPE;
+    }
+
+    @Override
+    protected void dispatch(Handler listener) {
+        listener.onClearActiveSelection(this);
+    }
+
+    public interface Handler extends EventHandler {
+        void onClearActiveSelection(ClearFinderSelectionEvent event);
+    }
+}
+
+
