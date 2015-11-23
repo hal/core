8b46af627b7baa6c97ebd7a515f582e07ae30f78
Heiko Braun <ike.braun@googlemail.com>
2015-11-23 09:47:26 +0100

    HAL-804: Change undertow to web/http

--- gui/src/main/java/org/jboss/as/console/client/shared/SubsystemMetaData.java
+++ gui/src/main/java/org/jboss/as/console/client/shared/SubsystemMetaData.java
@@ -80,7 +80,7 @@ public class SubsystemMetaData {
         groups.get(WEB).getItems().add(new SubsystemGroupItem("Web Services", "webservices"));
         groups.get(WEB).getItems().add(new SubsystemGroupItem("ModCluster", "modcluster", NameTokens.ModclusterPresenter));
 
-        groups.get(WEB).getItems().add(new SubsystemGroupItem("Undertow", "undertow", NameTokens.UndertowFinder));
+        groups.get(WEB).getItems().add(new SubsystemGroupItem("Web/HTTP - Undertow", "undertow", NameTokens.UndertowFinder));
         /*groups.get(WEB).getItems().add(new SubsystemGroupItem("Servlets", "undertow", NameTokens.ServletPresenter));
         groups.get(WEB).getItems().add(new SubsystemGroupItem("HTTP", "undertow", NameTokens.HttpPresenter));*/
         //groups.get(WEB).getItems().add(new SubsystemGroupItem("Undertow Core", "undertow", NameTokens.UndertowPresenter));
