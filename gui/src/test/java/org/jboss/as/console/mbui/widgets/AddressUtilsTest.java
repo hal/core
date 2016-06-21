81a5163e80dc51cc22d2421c32abe7330e582086
Tomas Hofman <thofman@redhat.com>
2016-06-21 14:48:57 +0200

    [JBEAP-5053] naming binding cannot be removed

---
+++ gui/src/test/java/org/jboss/as/console/mbui/widgets/AddressUtilsTest.java
@@ -0,0 +1,52 @@
+package org.jboss.as.console.mbui.widgets;
+
+import org.jboss.dmr.client.ModelNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Tomas Hofman (thofman@redhat.com)
+ */
+public class AddressUtilsTest {
+
+    @Test
+    public void testToString() {
+        ModelNode address = new ModelNode();
+        address.add("subsystem", "test");
+        address.add("resource", "name");
+
+        String s = AddressUtils.toString(address, true);
+        Assert.assertEquals("subsystem=test/resource=name", s);
+    }
+
+    @Test
+    public void testToStringValueContainsSlash() {
+        ModelNode address = new ModelNode();
+        address.add("subsystem", "test");
+        address.add("resource", "java:/global/a");
+
+        String s = AddressUtils.toString(address, true);
+        Assert.assertEquals("subsystem=test/resource=java\\:\\/global\\/a", s);
+    }
+
+    @Test
+    public void testAsKey() {
+        ModelNode address = new ModelNode();
+        address.add("subsystem", "test");
+        address.add("resource", "name");
+
+        String s = AddressUtils.asKey(address, true);
+        Assert.assertEquals("subsystem=test/resource=name", s);
+    }
+
+    @Test
+    public void testAsKeyValueContainsSlash() {
+        ModelNode address = new ModelNode();
+        address.add("subsystem", "test");
+        address.add("resource", "java:/global/a");
+
+        String s = AddressUtils.asKey(address, true);
+        Assert.assertEquals("subsystem=test/resource=java\\:\\/global\\/a", s);
+    }
+
+}
