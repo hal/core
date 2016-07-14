18e46a71ba0093ae565f06175c3109ec784f02dd
xstefank <mjolnir122@gmail.com>
2016-07-14 13:50:29 +0200

    [JBEAP-5040] - Remove JndiViewManagement interface

--- gui/src/main/java/org/jboss/as/console/client/shared/runtime/naming/JndiPresenter.java
+++ gui/src/main/java/org/jboss/as/console/client/shared/runtime/naming/JndiPresenter.java
@@ -52,7 +52,6 @@ import static org.jboss.dmr.client.ModelDescriptionConstants.*;
  * @date 7/20/11
  */
 public class JndiPresenter extends CircuitPresenter<JndiPresenter.MyView, JndiPresenter.MyProxy>
-    implements JndiViewManagement
 {
     @ProxyCodeSplit
     @NameToken(NameTokens.JndiPresenter)
@@ -113,7 +112,6 @@ public class JndiPresenter extends CircuitPresenter<JndiPresenter.MyView, JndiPr
         loadJndiTree();
     }
 
-    @Override
     public void refresh() {
         loadJndiTree();
     }
