e710009938f2bb09d9a9b4d7d9313ff771148247
Claudio Miranda <claudio@claudius.com.br>
2017-07-19 12:43:31 -0300

    HAL-1365 - Not possible to add Elytron policy resource using web console https://issues.jboss.org/browse/HAL-1365

--- gui/src/main/java/org/jboss/as/console/client/shared/subsys/elytron/ui/PolicyView.java
+++ gui/src/main/java/org/jboss/as/console/client/shared/subsys/elytron/ui/PolicyView.java
@@ -24,14 +24,20 @@ import org.jboss.as.console.client.shared.subsys.elytron.store.ElytronStore;
 import org.jboss.as.console.client.v3.dmr.AddressTemplate;
 import org.jboss.as.console.client.v3.dmr.ResourceDescription;
 import org.jboss.ballroom.client.rbac.SecurityContext;
+import org.jboss.dmr.client.ModelNode;
 import org.jboss.dmr.client.Property;
 import org.jboss.gwt.circuit.Dispatcher;
+
+import static org.jboss.dmr.client.ModelDescriptionConstants.NAME;
 
 /**
  * @author Claudio Miranda <claudio@redhat.com>
  */
 public class PolicyView extends ElytronGenericResourceView {
 
+    private static final String CUSTOM_POLICY = "custom-policy";
+    private static final String JACC_POLICY = "jacc-policy";
+    private static final String DEFAULT_POLICY = "default-policy";
     private GenericListComplexAttribute customPolicy;
     private GenericListComplexAttribute jaccPolicy;
 
@@ -40,18 +46,33 @@ public class PolicyView extends ElytronGenericResourceView {
             final SecurityContext securityContext, final String title,
             final AddressTemplate addressTemplate) {
         super(circuit, resourceDescription, securityContext, title, addressTemplate);
-        excludesFormAttributes("custom-policy", "jacc-policy");
+        excludesFormAttributes(CUSTOM_POLICY, JACC_POLICY);
 
     }
 
     @Override
     public Map<String, Widget> additionalTabDetails() {
         Map<String, Widget> additionalWidgets = new HashMap<>();
-        customPolicy = new GenericListComplexAttribute(circuit, resourceDescription, securityContext, ElytronStore.POLICY_ADDRESS, "custom-policy");
-        jaccPolicy = new GenericListComplexAttribute(circuit, resourceDescription, securityContext, ElytronStore.POLICY_ADDRESS, "jacc-policy");
+        customPolicy = new GenericListComplexAttribute(circuit, resourceDescription, securityContext,
+                ElytronStore.POLICY_ADDRESS, CUSTOM_POLICY);
+        jaccPolicy = new GenericListComplexAttribute(circuit, resourceDescription, securityContext,
+                ElytronStore.POLICY_ADDRESS, JACC_POLICY);
         additionalWidgets.put("Custom Policy", customPolicy.asWidget());
         additionalWidgets.put("JACC Policy", jaccPolicy.asWidget());
         return additionalWidgets;
+    }
+
+    protected void onAddCallbackBeforeSubmit(final ModelNode payload) {
+        ModelNode policyName = payload.get(NAME);
+        // set the defaul-policy if user didn't set in UI
+        if (!payload.hasDefined(DEFAULT_POLICY)) {
+            payload.get(DEFAULT_POLICY).set(policyName);
+        }
+        // add a policy to the list of jacc-policy.
+        // r-r-d doesn't requires a jacc-policy, but a policy resource cannot be added with no jacc-policy/custom-policy
+        ModelNode jaccPolicy = new ModelNode();
+        jaccPolicy.get(NAME).set(policyName);
+        payload.get(JACC_POLICY).add(jaccPolicy);
     }
 
     @Override
